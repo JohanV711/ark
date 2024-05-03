9719aed38ef086f5c0aacfaaf12574054a553cf1
Maru <nmcalderon01@gmail.com>
2024-05-03 20:43:26 +0200

    added sign out

--- app/src/main/java/com/cumn/ark/MainActivity.java
+++ app/src/main/java/com/cumn/ark/MainActivity.java
@@ -2,6 +2,8 @@ package com.cumn.ark;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -17,15 +19,17 @@ public class MainActivity extends AppCompatActivity {
     FirebaseUser user;
     FirebaseAuth auth;
     TextView textView;
+    Button buttonSignOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        buttonSignOut = findViewById(R.id.btn_signOut);
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
         textView = findViewById(R.id.user_details);
+
         if(user == null){
             Intent intent = new Intent(getApplicationContext(), Login.class);
             startActivity(intent);
@@ -33,6 +37,18 @@ public class MainActivity extends AppCompatActivity {
         }
         else{
             textView.setText(user.getEmail());
+            buttonSignOut.findViewById(R.id.btn_signOut);
+
+            buttonSignOut.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    FirebaseAuth.getInstance().signOut();
+                    Intent intent = new Intent(getApplicationContext(), Login.class);
+                    startActivity(intent);
+                    finish();
+                }
+            });
         }
+
     }
 }
--- app/src/main/java/com/cumn/ark/auth/Register.java
+++ app/src/main/java/com/cumn/ark/auth/Register.java
@@ -80,6 +80,9 @@ public class Register extends AppCompatActivity {
                                 if (task.isSuccessful()) {
                                    // FirebaseUser user = mAuth.getCurrentUser();
                                     Toast.makeText(Register.this, "Account created.", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), Login.class);
+                                    startActivity(intent);
+                                    finish();
                                 } else {
                                     Toast.makeText(Register.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
                                 }
