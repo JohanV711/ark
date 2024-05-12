9934eaf0f23308793b0f5e281674a15c4bf7c668
bs0431 <e.simbana@alumnos.upm.es>
2024-05-12 12:51:26 +0200

    modificado botones

--- app/src/main/java/com/cumn/ark/inicio.java
+++ app/src/main/java/com/cumn/ark/inicio.java
@@ -1,12 +1,30 @@
 package com.cumn.ark;
-
+import android.content.Intent;
 import android.os.Bundle;
+
+
+import android.view.View;
+
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class inicio extends AppCompatActivity {
+    Button buttonReg;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        buttonReg = findViewById(R.id.btn_register);
+        buttonReg.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), profile.class);
+                startActivity(intent);
+                finish();
+            }
+
+        });
+
     }
 }
