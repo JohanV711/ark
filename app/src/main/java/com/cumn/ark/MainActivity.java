b09f3f2ccbf7d3d8fe55d6029ec8827fc9fa5bf0
Maru <nmcalderon01@gmail.com>
2024-05-12 20:48:42 +0200

    main menu updated

--- app/src/main/java/com/cumn/ark/MainActivity.java
+++ app/src/main/java/com/cumn/ark/MainActivity.java
@@ -1,9 +1,11 @@
 package com.cumn.ark;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -19,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     FirebaseUser user;
     FirebaseAuth auth;
     TextView textView;
-    Button buttonSignOut;
+    ImageButton buttonSignOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
